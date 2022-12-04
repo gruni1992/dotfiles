4b2aa0c3c8d306803d044b8dbcfe9c86f42af9fa
Tobias Grunwald <tobias.grunwald@lise.de>
2022-12-04 15:12:18 +0100

    polybar wip

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+left=$(xrandr --listactivemonitors | grep -v eDP-1 | grep -v "Monitors" | sort -t"+" -nk3 | cut -d" " -f6 | sed -n 1p)
+right=$(xrandr --listactivemonitors | grep -v eDP-1 | grep -v "Monitors" | sort -t"+" -nk3 | cut -d" " -f6 | sed -n 2p)
+MONITOR=$left polybar left &
+MONITOR=$right polybar right &
+
