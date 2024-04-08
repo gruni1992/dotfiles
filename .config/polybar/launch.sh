b109c89ef1585de7f9c14bce885d6a05f80a4067
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2024-04-08 12:02:13 +0200

    merge with local home setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,8 +7,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-left=$(xrandr --listactivemonitors | grep -v eDP-1 | grep -v "Monitors" | sort -t"+" -nk3 | cut -d" " -f6 | sed -n 1p)
-right=$(xrandr --listactivemonitors | grep -v eDP-1 | grep -v "Monitors" | sort -t"+" -nk3 | cut -d" " -f6 | sed -n 2p)
-MONITOR=$left polybar left &
-MONITOR=$right polybar right &
+# left=$(xrandr --listactivemonitors | grep -v eDP-1 | grep -v "Monitors" | sort -t"+" -nk3 | cut -d" " -f6 | sed -n 1p)
+# right=$(xrandr --listactivemonitors | grep -v eDP-1 | grep -v "Monitors" | sort -t"+" -nk3 | cut -d" " -f6 | sed -n 2p)
+# MONITOR=$left polybar left &
+# MONITOR=$right polybar right &
+
+combined=$(xrandr --listactivemonitors | grep -v eDP-1 | grep -v "Monitors" | sort -t"+" -nk3 | cut -d" " -f6 | sed -n 2p)
+MONITOR=$combined polybar -c $HOME/.config/polybar/config.ini combined &
 
