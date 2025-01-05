f10f99dc2c7ea03b693cac18d99f7ed2c9c2f41a
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2025-01-05 14:00:31 +0100

    config changed

--- .config/fish/functions/functions.fish
+++ .config/fish/functions/functions.fish
@@ -34,3 +34,12 @@ function copyfile
     mkdir -p (dirname $argv[2])
     command cp $argv
 end
+
+function cdpr
+    set -l root (pwd | gsed -nE 's|(.*?/Projects/[^/]+).*|\1|')
+    if test -n "$root"
+        cd "$root"
+    else
+        echo "Not inside a Projects folder"
+    end
+end
