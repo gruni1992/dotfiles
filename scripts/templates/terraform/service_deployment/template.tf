b0d1ad1a2a5bf7cee077e1666e33381508a1f06c
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2025-03-29 19:36:59 +0100

    config changed

--- scripts/templates/terraform/service_deployment/template.tf
+++ scripts/templates/terraform/service_deployment/template.tf
@@ -1,26 +1,9 @@
-variable "app_name" {
-  type = string
-}
-
-variable "namespace" {
-  type = string
-}
-
-variable "name" {
-  type = string
-}
-
-variable "image" {
-  type = string
-}
-
-variable "port" {
-  type = number
-}
-
-variable "target_port" {
-  type = number
-}
+variable "app_name" { type = string }
+variable "namespace" { type = string }
+variable "name" { type = string }
+variable "image" { type = string }
+variable "port" { type = number }
+variable "target_port" { type = number }
 
 locals {
   name = var.name
